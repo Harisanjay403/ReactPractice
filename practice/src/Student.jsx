ec2c68e77620bd4f7d1b4aaf2ef35832dbd90a55
Harisanjay403 <168261655+Harisanjay403@users.noreply.github.com>
2024-08-21 07:29:06 +0000

    commit

---
+++ practice/src/Student.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export const Student = () => {
+  return (
+    <div className="student">
+        <table>
+            <tbody>
+                <tr>
+                    <th>Name</th>
+                    <td></td>
+                </tr>
+
+                <tr>
+                    <th>Age</th>
+                    <td></td>
+                </tr>
+
+                <tr>
+                    <th>Is Married</th>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    
+    </div>
+  )
+}
