c060e83e0aa66db4db2346f777272cd153b03a95
Harisanjay403 <168261655+Harisanjay403@users.noreply.github.com>
2024-08-22 11:32:14 +0000

    commit

---
+++ UserCard/src/UserCard.jsx
@@ -0,0 +1,46 @@
+import '../src/UserCard.css'
+
+function User(){
+    return(
+        <>
+        <div className="container">
+
+            <span className='isOnline'>Online</span>
+
+            <img className='img' src="https://www.shutterstock.com/image-photo/photo-document-passport-id-mature-260nw-1178817271.jpg" />
+
+            <h2 className='name'>Name of the employee</h2>
+            <h3 className='city'>District</h3>
+            <h4 className='desig'>Designation </h4>
+            
+            <div className='btn'>
+            <button>Message</button>
+            <button>Following</button>
+
+            </div>
+            
+
+            <div className="skills">
+                <h4 className='skills_heading'>Skills</h4>
+                <ul>
+                    <li>HTML </li>
+                    <li>CSS </li>
+                    <li>Java Script</li>
+                    <li>React </li>
+                    <li>Redux</li>
+                </ul>
+            </div>
+
+        </div>
+            
+        </>
+    )
+}
+
+export const UserCard = () => {
+  return (
+    <div>
+        < User />
+    </div>
+  )
+}
