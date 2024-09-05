93129c34ca950d8625d5b891303a817fc2040f40
Harisanjay403 <168261655+Harisanjay403@users.noreply.github.com>
2024-09-05 10:53:40 +0000

    commit

--- weatherApp/src/WeatherApp.jsx
+++ weatherApp/src/WeatherApp.jsx
@@ -33,6 +33,7 @@ const WeatherDetail=({weatherimg,temp,city,country,lat,lon,humidity,wind})=>{
 }
 
 export const WeatherApp = () => {
+    const [cityName,setCityName]=useState("Chennai")
     const [weatherimg,setWeatherimg]=useState(clearSun);
     const [temp,setTemp]=useState(0);
     const [city, setCity]=useState("Chennai")
@@ -41,10 +42,41 @@ export const WeatherApp = () => {
     const [lon,setLon]=useState(34.864)
     const [humidity,setHumidity]=useState(0)
     const [wind,setWind]=useState(30)
+    const [loading,setLoading]=useState(false)
+    const [cityNotFound,setCityNotFound]=useState(false)
+
+    const search = async()=>{
+        setLoading(true)
+        
+        const url=`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=7aa3031291ef2e98891e4647c3190ea5`
+
+        try{
+            let res = await fetch(url)
+            let urlDetail =res.json()
+            console.log(urlDetail)
+
+        }catch(error){
+
+        }finally{
+            setLoading(false)
+        }
+        let res = await fetch(url)
+        let urlDetail =res.json()
+    }
+
+    const handleCityName =(e)=>{
+        setCityName(e.target.value)
+    }
+
+    const handleSearch=(e)=>{
+        if(e.key === "enter"){
+            search()
+        }
+    }
   return (
     <div className='container'>
         <div className='city-input'>
-            <input type="text" placeholder='Search the city' /><span><img src="https://static.vecteezy.com/system/resources/thumbnails/014/440/989/small/search-black-shadow-icon-socialicon-set-png.png" alt="search-icon" /></span>
+            <input type="text" placeholder='Search the city' onChange={handleCityName} value={cityName} onKeyDown={handleSearch}/><span><img src="https://static.vecteezy.com/system/resources/thumbnails/014/440/989/small/search-black-shadow-icon-socialicon-set-png.png" alt="search-icon" onClick={()=>{search()}} /></span>
         </div>
 
         < WeatherDetail weatherimg={weatherimg} temp={temp} city={city} country={country} lat={lat} lon={lon} humidity={humidity} wind={wind}/>
