db192d3aec14ab2a976efc34f9b10982eaad52b3
Harisanjay403 <harisanjay403@gmail.com>
2024-09-16 07:34:14 +0530

    commit

--- passwordGenerator/src/PasswordGenerator.jsx
+++ passwordGenerator/src/PasswordGenerator.jsx
@@ -1,6 +1,32 @@
 
+import { useState } from 'react'
 import './PasswordGenerator.css'
 export const PasswordGenerator = ()=>{
+    const [upperCase,setUpperCase]=useState(true)
+    const [lowerCase,setLowerCase]=useState(true)
+    const [symbol,setSymbol]=useState(true)
+    const [number,setNumber]=useState(true)
+    const [length,setLength]=useState()
+
+    const handleLength = (e) =>{
+        setLength(parseInt(e.target.value))
+    }
+
+    let charset = ""
+
+    if(upperCase){
+        charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    }
+    if(lowerCase){
+        charset += "abcdefghijklmnopqrstuvwxyz"
+    }
+    if(symbol){
+        charset += "!@#$%^&*"
+    }
+    if(number){
+        charset += "012345679"
+    }
+    console.log(charset)
 
     return(
         <>
@@ -9,26 +35,30 @@ export const PasswordGenerator = ()=>{
             
                 <div className="input-data">
                     <label htmlFor="input-length">Password Length</label>
-                    <input type="number" id="input-length" />
+                    <input type="number" id="input-length" value={length} onChange={handleLength} />
                 </div>
 
                 <div className="include-group">
-                    <input type="checkbox" id="uppercase" />
+                    <input 
+                        type="checkbox" 
+                        id="uppercase" 
+                        checked={upperCase} 
+                        onChange={(e)=>setUpperCase(e.target.checked)}/>
                     <label htmlFor="uppercase"> Include Upper Case</label>
                 </div>
 
                 <div className="include-group">
-                    <input type="checkbox" id="lowercase" />
+                        <input type="checkbox" id="lowercase" checked={lowerCase}  onChange={(e)=>setLowerCase(e.target.checked)}/>
                     <label htmlFor="lowercase"> Include Lower Case</label>
                 </div>
 
                 <div className="include-group">
-                    <input type="checkbox" id="number" />
+                    <input type="checkbox" id="number" checked={number}  onChange={(e)=>setNumber(e.target.checked)}/>
                     <label htmlFor="number"> Include number </label>
                 </div>
 
                 <div className="include-group">
-                    <input type="checkbox" id="symbol" />
+                    <input type="checkbox" id="symbol" checked={symbol}  onChange={(e)=>setSymbol(e.target.checked)} />
                     <label htmlFor="symbol"> Include symbol </label>
                 </div>
 
