2057a22e559c25f0199485bbd2906346226cc7cb
Harisanjay403 <168261655+Harisanjay403@users.noreply.github.com>
2024-09-18 05:14:11 +0000

    header finished

--- Calender/src/Calender.jsx
+++ Calender/src/Calender.jsx
@@ -1,14 +1,26 @@
+import { useState } from 'react'
 import './Calender.css'
 
 export const Calender = () => {
+
+    const [currentDate,setCurrentDate] = useState(new Date())
+
+    const months=['January', "February", "March", "April", "May", "June", "July", "August", "September", "November", "December"]
 
     return(
         <>
             <div className="calender">
                 <div className="header">
                     <button>&lt;</button>
-                    <select></select>
-                    <select></select>
+
+                    <select className='months' value={months[currentDate.getMonth()]} >
+                        {months.map((month,intex)=>(<option value={month} key={intex} >{month}</option>))}
+                    </select>
+
+                    <select className='years' value={currentDate.getFullYear()}>
+                        {Array.from({length:10}, (_,i)=> currentDate.getFullYear()-5+i).map((year)=>(<option value={year} key={year} >{year}</option>))}
+                    </select>
+
                     <button>&gt;</button>
                 </div>
             </div>
