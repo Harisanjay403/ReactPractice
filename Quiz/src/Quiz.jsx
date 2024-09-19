702f998fd23422a9c838a0eeddc2012c9af08ccf
Harisanjay403 <168261655+Harisanjay403@users.noreply.github.com>
2024-09-19 06:40:56 +0000

    started

---
+++ Quiz/src/Quiz.jsx
@@ -0,0 +1,30 @@
+import "./Quiz.css"
+
+export const Quiz = () => {
+     
+    return (
+        <>
+            <div className="container">
+                
+                <div className="score-section">
+                    <h2> Your Score is : 3/4</h2>
+                    <button> Restart </button>
+
+                </div>
+
+                <div className="question-section">
+                    <h2> Question No.: 2</h2>
+                    <p> This is the sample question</p>
+                    <div className="option-btn">
+                        <button>option1</button>
+                        <button>option2</button>
+                        <button>option3</button>
+                        <button>option4</button>
+                    </div>
+                    <p> The time left is <span>5s</span></p>
+                </div>
+            </div>
+        </>
+    )
+
+}
