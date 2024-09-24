74f01b4eeb777752b497ec07041facfd52c38984
Harisanjay403 <168261655+Harisanjay403@users.noreply.github.com>
2024-09-24 09:36:16 +0000

    finished

--- practice/useReducer/src/AddItem.jsx
+++ practice/useReducer/src/AddItem.jsx
@@ -8,6 +8,9 @@ const reducer = (items,action)=>{
         
         case "DELETE_ITEM":
             return items.filter((item)=>(item.id !== action.payLoad))
+
+        case "UPDATE_ITEM":
+            return items.map((item)=>(item.id===action.payLoad.id ? {...item,name:action.payLoad.newName}: item))
     }
 }
 
@@ -24,7 +27,9 @@ export const AddItem = () =>{
             <ul>
                 {items.map((item,index)=>(
                     <li key={index}>{item.name}                     
-                    <button>Update</button>
+                    
+                    <button onClick={()=>{dispatch({type:"UPDATE_ITEM",payLoad:{id:item.id,newName:prompt("Update the Item",item.name)} })}}>Update</button>
+                    
                     <button onClick={()=>{dispatch({type:"DELETE_ITEM",payLoad:item.id})}}>Delete</button>
                     </li>
                     
