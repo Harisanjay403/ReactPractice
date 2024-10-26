0e9c3f103f0a90fe3f618ff16f46810ac554ea72
Harisanjay403 <168261655+Harisanjay403@users.noreply.github.com>
2024-10-26 11:06:17 +0530

    Update Project.jsx

--- portfolio/src/Project.jsx
+++ portfolio/src/Project.jsx
@@ -150,27 +150,112 @@ export const Project = () =>{
 
                 </div>
 
-                <div className="project">
+               <div className="project">
                     <div className="img-container">
                         <div className="project-img">
-                            <img src="./src/assets/password.JPG" alt="Image" />
+                            <img src="./src/assets/qrcodeGenerator.JPG" alt="Image" />
                         </div>
 
                         <div className="project-button">
-                            <a href="https://github.com/Harisanjay403/ReactPractice/tree/main/passwordGenerator/src" ><button>Github-Code</button></a>
-                            <a href="https://password-generator-harisanjay.web.app/"><button>Website</button></a>
+                            <a href="https://github.com/Harisanjay403/ReactPractice/tree/main/QrCodeGenerator/src" ><button>Github-Code</button></a>
+                            <a href="https://qr-code-generator---harisanjay.web.app/"><button>Website</button></a>
                         </div>
 
                     </div>           
                     
                     <div className="project-content">
-                        <h1>Strong Password Generator</h1>
-                        <p>The PasswordGenerator component allows users to create customizable passwords. Users can select options for uppercase letters, lowercase letters, numbers, and symbols, as well as specify the password length. When the "Generate Password" button is clicked, a random password is created based on these settings. The generated password can be copied to the clipboard with an alert confirmation. The component uses React hooks for state management and updates, along with Bootstrap for styling and layout.</p>
+                        <h1>QR Code Generator</h1>
+                        <p>It's a React application that generates QR codes. It uses useState to manage state for the QR code image, loading status, data input, and image size. Users can give input data and size, and pressing "Enter" or clicking a button generates the QR code by fetching it from an API. The component also includes a download feature to save the generated QR code. The layout is styled with Bootstrap, enhancing its visual appeal.</p>
                     </div> 
 
                                       
 
                 </div>
+
+                <div className="project">
+                    <div className="img-container">
+                        <div className="project-img">
+                            <img src="./src/assets/mcq.JPG" alt="Image" />
+                        </div>
+
+                        <div className="project-button">
+                            <a href="https://github.com/Harisanjay403/ReactPractice/tree/main/Quiz/src" ><button>Github-Code</button></a>
+                            <a href="https://mcq-harisanjay-rajan.web.app/"><button>Website</button></a>
+                        </div>
+
+                    </div>           
+                    
+                    <div className="project-content">
+                        <h1>Multiple Choice Question</h1>
+                        <p>
+                        The MCQ application is a React-based quiz interface. It uses useState to manage the score, current question index, and whether to show the score. Users answer multiple-choice questions, with correct answers updating the score. The interface displays one question at a time, allowing navigation through questions and showing the score at the end. Users can restart the MCQ, and questions are defined in a hardcoded array, including their options and correct answers. The layout is styled with Bootstrap.</p>
+                    </div>                                    
+
+                </div>
+                <div className="project">
+                    <div className="img-container">
+                        <div className="project-img">
+                            <img src="./src/assets/registration.JPG" alt="Image" />
+                        </div>
+
+                        <div className="project-button">
+                            <a href="https://github.com/Harisanjay403/ReactPractice/tree/main/Registration/src" ><button>Github-Code</button></a>
+                            <a href="https://registration-form-harisanjay-r.web.app/"><button>Website</button></a>
+                        </div>
+
+                    </div>           
+                    
+                    <div className="project-content">
+                        <h1>Simple Registration Form</h1>
+                        <p>The registration form  shows user details like name, age, gender, marital status, country, and bio in a table. Users can update their information through input fields for name and age, radio buttons for gender selection, and a checkbox for marital status. A dropdown allows users to select their country, and a textarea is provided for entering a personal bio. Changes are managed with the handleChange function, keeping the displayed data in sync with user inputs.</p>
+                    </div>                                    
+
+                </div>
+                <div className="project">
+                    <div className="img-container">
+                        <div className="project-img">
+                            <img src="./src/assets/usercard.JPG" alt="Image" />
+                        </div>
+
+                        <div className="project-button">
+                            <a href="https://github.com/Harisanjay403/ReactPractice/tree/main/UserCard/src" ><button>Github-Code</button></a>
+                            <a href="https://user-card-harisanjay-rajan.web.app/"><button>Website</button></a>
+                        </div>
+
+                    </div>           
+                    
+                    <div className="project-content">
+                        <h1>User Card</h1>
+                        <p>The UserCard displays a list of user profiles, each represented by the User subcomponent. Each user card shows online status, an image, name, city, designation, and a list of skills. The online status is visually indicated with a class that changes based on the user's availability. Each card includes "Message" and "Following" buttons. Users are rendered in a responsive grid layout using Bootstrap's Container and Row components, ensuring a clean and organized presentation of user data.</p>
+                    </div>                                    
+
+                </div>
+
+                <div className="project">
+                    <div className="img-container">
+                        <div className="project-img">
+                            <img src="./src/assets/weather.JPG" alt="Image" />
+                        </div>
+
+                        <div className="project-button">
+                            <a href="https://github.com/Harisanjay403/ReactPractice/tree/main/weatherApp/src" ><button>Github-Code</button></a>
+                            <a href="https://weather-app-harisanjay-rajan.web.app/"><button>Website</button></a>
+                        </div>
+
+                    </div>           
+                    
+                    <div className="project-content">
+                        <h1>Weather App</h1>
+                        <p>The WeatherApp component fetches and displays weather data for a specified city using the OpenWeatherMap API. It manages state for various weather attributes like temperature, humidity, wind speed, and conditions, updating based on user input. The WeatherDetail subcomponent visually presents this data alongside an appropriate weather image. Users can search for cities, and the app handles loading states and errors if a city isn't found. The design incorporates Bootstrap for layout and includes responsive input handling for seamless interaction.</p>
+                    </div>                                    
+
+                </div>
+
+                
+            </div>
+        </>
+    )
+}
 
                 
             </div>
