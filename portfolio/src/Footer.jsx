ae6da18a71e5a095c60a926f1d25f867e2904466
Harisanjay403 <168261655+Harisanjay403@users.noreply.github.com>
2024-10-28 13:03:22 +0530

    Update Footer.jsx

--- portfolio/src/Footer.jsx
+++ portfolio/src/Footer.jsx
@@ -1,10 +1,33 @@
 import './Footer.css'
+import { Link } from 'react-router-dom'
 
 export const Footer = () =>{
 
     return(
         <>
             <div className="footer-container">
+                <h1>Harisanjy Rajan</h1>
+
+                <div className="lists">
+                    <ul>
+                    <li>
+                            <Link to={"/"}>Home</Link>
+                        </li>
+                        <li>
+                            <Link to={"/project"}>Project</Link>
+                        </li>
+                        <li>
+                            <Link to={"/resume"}>Resume</Link>
+                        </li>
+                        <li>
+                            <Link to={"/contact"}>Contact</Link>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="logos">
+                    
+                </div>
                 
             </div>
         </>
