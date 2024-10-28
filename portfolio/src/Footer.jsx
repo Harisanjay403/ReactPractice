e6a525409420bba2b50f592e5ca14db59ddd8976
Harisanjay403 <168261655+Harisanjay403@users.noreply.github.com>
2024-10-28 16:27:40 +0530

    footer completed

--- portfolio/src/Footer.jsx
+++ portfolio/src/Footer.jsx
@@ -26,7 +26,17 @@ export const Footer = () =>{
                 </div>
 
                 <div className="logos">
+
+                    <Link to={'https://www.linkedin.com/in/harisanjay-rajan-hari62'}> <img src='./src/assets/linkedin.png' alt="" /></Link>
+
+                    <Link to={'https://github.com/Harisanjay403/ReactPractice'}> <img src='./src/assets/github-sign.png' alt="" /></Link>
                     
+                    <Link to={'https://wa.me/9688616162'}> <img src='./src/assets/whatsapp.png' alt="" /></Link>
+                    
+                </div>
+
+                <div className="designedby">
+                    <p>Designed by Harisanjay</p>
                 </div>
                 
             </div>
