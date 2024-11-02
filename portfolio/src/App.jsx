a0e1b5be4217c55c103c81cc44694a967ad94566
Harisanjay403 <168261655+Harisanjay403@users.noreply.github.com>
2024-11-02 10:32:23 +0000

    contact page editing

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -5,6 +5,7 @@ import { Home } from './Home.jsx'
 import { Project } from './Project.jsx'
 import { Resume } from './Resume.jsx'
 import { Footer } from './Footer.jsx'
+import { Contact } from './Contact.jsx'
 
 function App() {
   
@@ -17,6 +18,7 @@ function App() {
         <Route path={'/'} element={< Home />}/>
         <Route path={'/project'} element={ < Project />} />
         <Route path={'/resume'} element={ < Resume />} />
+        <Route path={'/contact'} element={ <Contact />}/>
           
         
       </Routes>
---
+++ portfolio/src/Contact.jsx
@@ -0,0 +1,34 @@
+import "./Contact.css"
+
+export const Contact = () =>{
+
+    return(
+        <>
+            <div className="contact-container">
+                <div className="contacts">
+                    <div className="address">                     
+                        <div class="loader"></div>
+                        <p>Ramanathapuram,<br />Coimbatore-641036 <br />Tamil Nadu <br /> India</p>
+                    </div>
+
+                    <div className="address">                     
+                        <div class="loader"></div>
+                        <p>Ramanathapuram,<br />Coimbatore-641036 <br />Tamil Nadu <br /> India</p>
+                    </div>
+
+                    <div className="address">                     
+                        <div class="loader"></div>
+                        <p>Ramanathapuram,<br />Coimbatore-641036 <br />Tamil Nadu <br /> India</p>
+                    </div>
+
+                    <div className="address">                     
+                        <div class="loader"></div>
+                        <p>Ramanathapuram,<br />Coimbatore-641036 <br />Tamil Nadu <br /> India</p>
+                    </div>
+
+                    
+                </div>
+            </div>
+        </>
+    )
+}
