488e3c1c805d770a49af91e23e45b456fc134250
Harisanjay403 <168261655+Harisanjay403@users.noreply.github.com>
2024-11-14 11:06:47 +0000

    responsive website

--- portfolio/src/Resume.jsx
+++ portfolio/src/Resume.jsx
@@ -1,8 +1,15 @@
+import "./Resume.css"
+import bmi from './assets/bg_img.jpg'
+import pdf from './assets/HarisanjayRajanResume.pdf'
+import HarisanjayRajanResume from './assets/HarisanjayRajanResume.jpg'
 export const Resume = () =>{
      
     return (
         <>
-            This is resume page
+            <div className="resume-container">
+                <img src={HarisanjayRajanResume} alt="Resume" />
+                <a href={pdf} download>Download Resume</a>
+            </div>
         </>
     )
 }
