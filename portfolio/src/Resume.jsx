606c111053a99d611a036bffacb5b3df8ec89eff
Harisanjay403 <168261655+Harisanjay403@users.noreply.github.com>
2024-11-15 05:36:12 +0000

    responsive website

--- portfolio/src/Resume.jsx
+++ portfolio/src/Resume.jsx
@@ -2,14 +2,22 @@ import "./Resume.css"
 import bmi from './assets/bg_img.jpg'
 import pdf from './assets/HarisanjayRajanResume.pdf'
 import HarisanjayRajanResume from './assets/HarisanjayRajanResume.jpg'
+import { Container, Row, Col, Image} from 'react-bootstrap'
 export const Resume = () =>{
      
     return (
         <>
-            <div className="resume-container">
-                <img src={HarisanjayRajanResume} alt="Resume" />
-                <a href={pdf} download>Download Resume</a>
-            </div>
+        <Container>
+            <Row>
+                <Col xs={12}>
+                    <div className="resume-container">
+                        <img src={HarisanjayRajanResume} alt="Resume" />
+                        <a href={pdf} download>Download Resume</a>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+            
         </>
     )
 }
